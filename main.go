af036dca98d91e248e7871367337382844be9fee
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-05-21 13:25:05 +0300

    Added basic storage implementation.(Deletion doesn't work)

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+	"fake-klasse/storage"
+)
+
+func main() {
+	
+	storage := storage.Storage{}
+	
+	storage.Init("School.db")
+	
+	defer storage.Close()
+
+	storage.DeleteAllStudents()
+	
+	
+	storage.AddStudent("Ezitis", "Migla")
+
+	storage.AddStudent("Grizzly", "Bear")
+
+	storage.PrintStudents()	
+
+	storage.DeleteStudent(2)
+
+	
+
+	storage.PrintStudents()	
+}
---
+++ storage/storage.go
@@ -0,0 +1,102 @@
+package storage
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "modernc.org/sqlite"
+)
+
+var StudentsSchema = `CREATE TABLE IF NOT EXISTS students (
+								id		INTEGER PRIMARY KEY AUTOINCREMENT,
+							    name	TEXT,
+							    surname	TEXT
+								);`
+var selectStudents = `SELECT id, name, surname FROM students`
+var insertStudent = `INSERT INTO students (name, surname) VALUES(?,?)`
+var deleteAllStudents = `DELETE FROM students`
+var deleteStudentByID = `DELETE FROM students WHERE id IN (?)`
+
+type Student struct{ // UPPERCASE!!!
+	// Id int
+	Name    string `db:"name"`
+	Surname string `db:"surname"`
+}
+
+type Storage struct{
+	db *sqlx.DB
+}
+
+func (storage *Storage) Init(path string) {
+	// Creating and/or opening DB:
+	db, err := sqlx.Open("sqlite", path)
+	if err != nil {
+		log.Fatal("failed to open SQLite DB: ", db)
+	}
+	storage.db = db
+
+	// Creating tables if they don't exist:
+	storage.CreateTable(StudentsSchema)
+}
+func (storage *Storage) CreateTable(schema string) {
+	// Creating tables if they don't exist:
+	result, err := storage.db.Exec(schema)
+	if err != nil {
+		log.Fatal("table creation failed. Query: ", schema, "\nError:", err)
+	}
+	if count, err := result.RowsAffected(); err != nil{
+		log.Fatal(count," rows affected.")
+	}
+}
+func (storage *Storage) Close(){ // To control flow in other files.
+	storage.db.Close()
+}
+func (storage *Storage) AddStudent(name, surname string) {
+	result, err := storage.db.Exec(insertStudent, name, surname)
+	if err != nil {
+		log.Fatal("table creation failed. Query: ", insertStudent, "\nError:", err)
+	}
+	if count, err := result.RowsAffected(); err != nil {
+		log.Fatal(count, " rows affected.")
+	}
+}
+func (storage *Storage) GetStudents() (*[]Student){// Writes all the students from table to slice.
+
+	var students []Student
+
+	if err := storage.db.Select(&students, selectStudents); err != nil{
+		log.Fatal("querying 'students' table failed. Query: ", selectStudents,"\nError: ",  err)
+	}
+
+	return &students
+}
+func (storage *Storage) PrintStudents() {
+	// Getting students:
+	students := storage.GetStudents()
+
+	// Printing:
+	for _,student := range *students{
+		fmt.Print(/*student.Id, ": ",*/ student.Name, " ", student.Surname, "\n")
+	}
+}
+func (storage *Storage) DeleteAllStudents() {
+	result, err := storage.db.Exec(deleteAllStudents)
+	if err != nil {
+		log.Fatal("student deletion failed. Query: ", deleteAllStudents, "\nError:", err)
+	}
+	if count, err := result.RowsAffected(); err != nil {
+		log.Fatal(count, " rows affected.")
+	}
+}
+func (storage *Storage) DeleteStudent(id int) {
+	result, err := storage.db.Exec(deleteStudentByID, id)
+	if err != nil {
+		log.Fatal("student deletion failed. Query: ", deleteStudentByID, "\nError:", err)
+	}
+	if count, err := result.RowsAffected(); err != nil {
+		log.Fatal(count, " rows affected.")
+	}
+
+
+}
